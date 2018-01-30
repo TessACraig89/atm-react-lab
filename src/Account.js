2d739fa4807c52759c0387e55bf7ce023e8b04a3
Tess Craig <tessashleycraig89@gmail.com>
2018-01-29 23:42:04 -0600

    passes a name property to each Account component for checkings and savings

--- src/Account.js
+++ src/Account.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 export default class Account extends Component {
   render() {
     return (
+      // Use the property you set in App.js to add the name of the account to the <h2>
       <div className="account">
         <h2>TODO: CHANGE TO PROP</h2>
         <div className="balance">$0</div>
--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ class App extends Component {
           <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
           <div id="title">Bank of GA</div>
         </div>
-        <Account />
-        <Account />
+        {/* Pass a name property to each Account component, one for "Checking", the other for "Savings". These will be used and accessed as props for our component. */}
+        <Account name="Checking"/>
+        <Account name="Savings"/>
         <div className="clear"></div>
       </div>
     );
