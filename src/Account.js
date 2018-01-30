eabb8e321238e84bf300c36d855dc1cb7befe72b
Tess Craig <tessashleycraig89@gmail.com>
2018-01-30 00:11:28 -0600

    Adds a click handler in input tags in JSX return block and defines a click handler method within the Account class so that when the Deposit button is clicked, the amount entered in the text field is added to the balance

--- src/Account.js
+++ src/Account.js
@@ -9,6 +9,21 @@ export default class Account extends Component {
           }
         }
     }
+    // Define a click handler method within the Account class
+    handleDepositClick(e) {
+       // It is good practice to still prevent default behavior
+       e.preventDefault();
+       // set a local variable to the amount entered in the text box.
+       let amount = parseInt(this.inputBox.value);
+       // set a local variable to the new balance based off of the original balance + amount
+       let newBalance = this.state.balance + amount;
+       // set the balance to the newBalance using the setState method (necessary)
+       this.setState({
+         balance: newBalance
+       })
+       // empty out the text box in this component
+       this.inputBox.value = '';
+     }
   render() {
     return (
       // Use the property you set in App.js to add the name of the account to the <h2>
@@ -18,8 +33,9 @@ export default class Account extends Component {
       <div className="balance">$0</div>
       {/* Set a ref property on the text field, which is a callback function to save a reference to that text field in our Account object. This is the standard way we can access the data in the field later when we want to know what values to add/subtract from our account */}
       <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
-      <input type="button" value="Deposit" />
-      <input type="button" value="Withdrawl" />
+      {/* Add a click handler in your input tags in our JSX return block */}
+      <input type="button" value="Deposit" onClick={(e) => this.handleDepositClick(e)} />
+      <input type="button" value="Withdraw" onClick={(e) => this.handleWithdrawClick(e)} />
     </div>
     )
   }
