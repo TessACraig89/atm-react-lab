29f2d36c4cf6272d1db7645739f473f5ad3a582d
Tess Craig <tessashleycraig89@gmail.com>
2018-01-29 23:57:01 -0600

    uses property set in App.js to add the name of the acount to <h2>

--- src/Account.js
+++ src/Account.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 
 export default class Account extends Component {
+    //     constructor(props){
+    //       super(props)
+    //       this.state = {
+    //         balance: 0
+    //       }
+    //     }
+    // }
   render() {
     return (
       // Use the property you set in App.js to add the name of the account to the <h2>
       <div className="account">
-        <h2>TODO: CHANGE TO PROP</h2>
-        <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
-      </div>
+      {/* this.props.name is referring to the name property we assigned the App component in App.js */}
+      <h2>{this.props.name}</h2>
+      <div className="balance">$0</div>
+      <input type="text" placeholder="enter an amount" />
+      <input type="button" value="Deposit" />
+      <input type="button" value="Withdrawl" />
+    </div>
     )
   }
 }
