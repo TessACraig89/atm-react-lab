5221df651d95f15b2f8bb968bb0b41a56dcc8226
Tess Craig <tessashleycraig89@gmail.com>
2018-01-30 00:27:28 -0600

    sets the default balance for the balanceClass, if the balance is 0, then adds the class zero to balanceClass, replaces the hardcoded balance class with the balanceClass variable in return jsx code block

--- src/Account.js
+++ src/Account.js
@@ -41,12 +41,20 @@ export default class Account extends Component {
         this.inputBox.value = '';
       }
   render() {
+    //If the current balance is 0, you should add a class of zero to the <div className="balance">. You can complete these computations in the render method, but before the JSX portion is returned
+    // set the default class to `balance` for the balanceClass.
+    let balanceClass = 'balance';
+    // if the balance is 0, then add the class zero to balanceClass
+    if (this.state.balance === 0) {
+      balanceClass += ' zero';
+    }
     return (
       // Use the property you set in App.js to add the name of the account to the <h2>
       <div className="account">
       {/* this.props.name is referring to the name property we assigned the App component in App.js */}
       <h2>{this.props.name}</h2>
-      <div className="balance">$0</div>
+      {/* Replace the hardcoded `balance` class with the balanceClass variable in your return jsx code block */}
+      <div className={balanceClass}>$0</div>
       {/* Set a ref property on the text field, which is a callback function to save a reference to that text field in our Account object. This is the standard way we can access the data in the field later when we want to know what values to add/subtract from our account */}
       <input type="text" placeholder="enter an amount" ref={(input) => this.inputBox = input} />
       {/* Add a click handler in your input tags in our JSX return block */}
